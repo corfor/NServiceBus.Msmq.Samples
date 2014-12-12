aaf782205b0a90f9aa3d79998642bb06347dc7a2
Corey Ford <corey@coreyford.com>
2014-12-12 09:06:13 -0600

    updated to latest nuget packages

--- VideoStore.Msmq/VideoStore.ECommerce/App_Start/RouteConfig.cs
+++ VideoStore.Msmq/VideoStore.ECommerce/App_Start/RouteConfig.cs
@@ -8,7 +8,7 @@
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.MapHubs();
+            //routes.MapHubs();
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute("Default", String.Empty, new { controller = "Home", action = "Index" });
         }
